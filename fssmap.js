3b7666b1c31e6c5bcbacad84100bccc35506b764
Neeva <sethineeva@gmail.com>
2021-10-17 16:27:09 -0400

    Markers
    Added markers for every location with a large solar power plant as labeled by https://www.fpl.com/energy-my-way/solar/energy-centers.html

--- fssmap.js
+++ fssmap.js
@@ -4,4 +4,141 @@ function initMap() {
         center: {lat: 28.0, lng: -83.5},
         zoom: 7
       });
+
+      //current solar panel location markers
+      var marker = new google.maps.Marker({
+        position:{lat:30.2808, lng:-82.7818},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.259140, lng:-82.837050},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.259140, lng:-82.837050},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.288440, lng:-82.191560},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.288440, lng:-82.191560},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.233200, lng:-82.257750},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.233200, lng:-82.257750},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.004050, lng:-82.289500},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:30.004050, lng:-81.676990},
+        map:map
+      })
+      //missing coral farms energy center addrerss
+      var marker = new google.maps.Marker({
+        position:{lat:29.888830, lng:-81.676990},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:29.618379, lng:-81.845289},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:29.761904, lng:-81.425689},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:29.0166941, lng:-81.1021556},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.611285, lng:-82.3484599},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.476435, lng:-82.278709},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.595426, lng:-82.272628},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:28.51514434814453, lng:-80.68545532226562},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.322106, lng:-81.810023},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.3220871, lng:-81.8095089},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.420316, lng:-80.945361},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.488056, lng:-80.559198},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.628402, lng:-80.578013},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.633105, lng:-80.569755},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.629709, lng:-80.50909},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.215949, lng:-80.36627},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.217952728271484, lng:-80.76224517822266},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.240274, lng:-80.54289},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:27.05709, lng:-80.4821286},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:26.490091, lng:-80.091634},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:26.774182, lng:-80.360039},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:26.70458, lng:-81.129041},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:26.771959, lng:-81.36811},
+        map:map
+      })
+      var marker = new google.maps.Marker({
+        position:{lat:25.6376471, lng:-80.4889484},
+        map:map
+      })
+
+
 }
